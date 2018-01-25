c5f6293015e0cc3b2341661e297b183b7e5eed77
yogeshTheCaptain <yogeshsati537@gmail.com>
2018-01-25 02:45:23 -0800

    Add all files

---
+++ app/src/main/java/com/example/yogesh/basicphrases1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.yogesh.basicphrases1;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonTapped(View view){
+
+        int id =view.getId();
+        String ourId ="";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId,"raw","com.example.yogesh.basicphrases1");
+        MediaPlayer mediaPlayer = MediaPlayer.create(this,resourceId);
+        mediaPlayer.start();
+
+    }
+}
